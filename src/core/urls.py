d16541850fba7ca6f292790ef5eae3e19b2cea10
mostafa20001379 <mostafa.ahoee1379@gmail.com>
2023-07-07 10:37:19 +0330

    add staticfiles to urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,3 +23,5 @@ urlpatterns = [
 
     path('docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
